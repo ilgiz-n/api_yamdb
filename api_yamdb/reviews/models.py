d3c1a134a12f1af7b4a9186749c4989e96ff7233
Никита Макареичев <nikita.mska@gmail.com>
2022-08-10 18:27:44 +0300

    Review and Comments models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+User = get_user_model()
+
+
+class Reviews(models.Model):
+    title = models.ForeignKey(
+        Titles,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Comments(models.Model):
+    review = models.ForeignKey(
+        Reviews,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
