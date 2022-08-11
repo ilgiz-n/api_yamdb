d42b2af558415f82ec33cecbc43b74838702a7db
Екатерина Русова <rusova@top-context.ru>
2022-08-11 14:41:09 +0300

    genres, categories, titles приложение api, корректировки вью

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -117,8 +117,3 @@ USER_LEVEL_CHOICES = (
         (2, "Moderator"),
         (3, "User"),
     )
-
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-}
