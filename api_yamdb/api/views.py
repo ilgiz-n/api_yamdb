445738bfce80a54bdd1ccdf8db52047043c1a402
Никита Макареичев <nikita.mska@gmail.com>
2022-08-16 11:10:21 +0300

    removed unnecessary imports

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import filters, viewsets
-from reviews.models import Review, Comments, Categories, Genres
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, permissions, status, viewsets
 from rest_framework.decorators import action
