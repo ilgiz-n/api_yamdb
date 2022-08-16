46a470d91bef81573c0f6c9a301bbb3017e1a2f1
Никита Макареичев <nikita.mska@gmail.com>
2022-08-16 19:16:04 +0300

    update review model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -98,7 +98,7 @@ class Review(models.Model):
         on_delete=models.CASCADE,
         related_name='reviews'
     )
-    score = models.IntegerField(
+    score = models.PositiveSmallIntegerField(
         validators=[
             MinValueValidator(1, 'Оценка должна быть не меньше 1.'),
             MaxValueValidator(10, 'Оценка должна быть не больше 10.')
@@ -107,7 +107,7 @@ class Review(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['pub_date']
+        ordering = ('pub_date',)
         constraints = [
             models.UniqueConstraint(
                 fields=['title', 'author'],
@@ -132,4 +132,4 @@ class Comments(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['pub_date']
+        ordering = ('pub_date',)
